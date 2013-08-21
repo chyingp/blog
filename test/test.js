82802d81d9287d4bc7710b2a0ee52f686c8098d4
chyingp <chyingp@gmail.com>
2013-08-21 21:52:00 +0800

    测试一下bower

---
+++ test/test.js
@@ -0,0 +1,10 @@
+/**package
+{ "name": "cyp-test",
+  "version": "0.1.0",
+  "description":"Just this one file, it's a whole package!" }
+**/
+var test = {
+  say:function(){
+    return 'test says';
+  }
+};
