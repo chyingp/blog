2f87e90dce52a264edf6c8b4f4ab839efa5c5855
casperchen <chyingp@gmail.com>
2014-07-15 01:46:09 +0800

    1、注释添加 2、bug fix：已存在目录的检测

--- demo/cheerio/demo.js
+++ demo/cheerio/demo.js
@@ -1,24 +1,29 @@
 /**
  * Created by a on 14-7-15.
  */
-var cheerio = require('cheerio'),
+var cheerio = require('cheerio'),   // 主角 cheerio
 	fs = require('fs'),
 	url = require('url'),
-	path = require('path'),
-	from = 'src/index.html',
-	to = 'dest/index.html',
+	path = require('path');
+
+var from = 'src/index.html',    // 源文件
+	to = 'dest/index.html', // 最终生成的文件
+	fromDir = path.dirname(from),
+	toDir = path.dirname(to),
 	content = fs.readFileSync(from),
-	$ = cheerio.load(content),
+	$ = cheerio.load(content),  // 加载源文件
 	fd = 0;
 
+// 选取 src/index.html 里所有的script标签，并将带有 __inline 标记的内嵌
 $('script').each(function(index, script){
 	var script = $(this),
 		src = script.attr('src'),
 		urlObj = url.parse(src),
-		dir = path.dirname(from),
-		pathname = path.resolve(dir, urlObj.pathname),
+		fromDir = path.dirname(from),
+		pathname = path.resolve(fromDir, urlObj.pathname),
 		scriptContent = '';
 
+	// 关键步骤：__inline 检测！（ps：非严谨写法）
 	if(urlObj.search.indexOf('__inline')!=-1){
 		scriptContent = fs.readFileSync(pathname);
 		script.replaceWith('<script>'+ scriptContent +'</script>');
@@ -26,13 +31,11 @@ $('script').each(function(index, script){
 });
 
 // 创建dest目录
-if(!fs.exists(path.dirname(to))){
-	fs.mkdirSync(path.dirname(to));
+if(!fs.existsSync(toDir)){
+	fs.mkdirSync(toDir);
 }
 
+// 将处理完的文件写回去
 fd = fs.openSync(to, 'w');
 fs.writeFileSync(to, $.html());
 fs.closeSync(fd);
-
-
-
