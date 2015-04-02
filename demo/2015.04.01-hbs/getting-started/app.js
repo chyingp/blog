467e394efcff28d09b95dc33ec21b7a3d89ff688
casperchen(陈映平) <chyingp@gmail.com>
2015-04-02 09:48:30 +0800

    不依赖express的demo

--- demo/2015.04.01-hbs/getting-started/app.js
+++ demo/2015.04.01-hbs/getting-started/app.js
@@ -3,14 +3,10 @@ var express = require('express'),
 	app = express();
 
 app.set('view engine', 'hbs');	// 用hbs作为模版引擎
-// app.engine('html', require('hbs').__express);	// 模版的文件名后缀为html
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/views');	// 模版所在路径
 
 app.get('/', function(req, res){
-    res.render('index', {
-    	title: 'hbs demo',
-    	author: 'chyingp'
-    });
+    res.render('index', {title: 'hbs demo', author: 'chyingp'});
 });
 
 app.listen(3000);
