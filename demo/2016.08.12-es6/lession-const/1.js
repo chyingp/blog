5f83cbf0ffd0dc3ff96a765883233fd4d7c54862
casperchen(陈映平) <chyingp@gmail.com>
2017-04-17 22:14:42 +0800

    const例子

---
+++ demo/2016.08.12-es6/lession-const/1.js
@@ -0,0 +1,4 @@
+// 不允许修改const声明的常量
+const PI = 3.14;
+
+PI = 3.15;  // TypeError: Assignment to constant variable.
---
+++ demo/2016.08.12-es6/lession-const/2.js
@@ -0,0 +1,4 @@
+// const声明的常量，一开始就必须赋值
+const PI;
+
+PI = 3.14;  // SyntaxError: Missing initializer in const declaration
