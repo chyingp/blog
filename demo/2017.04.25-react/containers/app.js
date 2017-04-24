d90bbc1dc4b155da3e16b153a9a3931d39e2c8a3
casperchen(陈映平) <chyingp@gmail.com>
2017-04-25 01:03:44 +0800

    新增react preset

--- demo/2017.04.25-react/containers/app.js
+++ demo/2017.04.25-react/containers/app.js
@@ -1,1 +1,14 @@
-document.getElementById('container').innerHTML = 'hello world';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	constructor (props) {
+		super(props);	
+	}
+
+	render () {
+		return <div>hello</div>;
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
--- demo/2017.04.25-react/webpack.config.js
+++ demo/2017.04.25-react/webpack.config.js
@@ -5,5 +5,10 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+	rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
   }
 };
