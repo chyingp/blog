b4503be1f6f829abd2c05cecc2fccc260446be48
casperchen(陈映平) <chyingp@gmail.com>
2017-05-03 22:52:23 +0800

    split-code 例子

---
+++ demo/2017.05.03-webpack/split-code/index.js
@@ -0,0 +1,7 @@
+var moment = require('moment');
+
+// document.getElementById('app').innerHTML = moment().format();
+
+console.log( moment().format() );
+
+console.log( 'hello' );
---
+++ demo/2017.05.03-webpack/split-code/webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = function(env) {
+    return {
+        entry: {
+            main: './index.js'
+        },
+        output: {
+            filename: '[name].[chunkhash].js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        plugins: [
+            new webpack.optimize.CommonsChunkPlugin({
+                names: 'vendor',
+                minChunks: function (module) {
+                   // this assumes your vendor imports exist in the node_modules directory
+                   return module.context && module.context.indexOf('node_modules') !== -1;
+                }
+            }),
+            //CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+            new webpack.optimize.CommonsChunkPlugin({ 
+                name: 'manifest' //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
+            })
+        ]
+    }
+}
