9b867a88325c041b92f58f73bf8845e88e54638e
casperchen(陈映平) <chyingp@gmail.com>
2017-06-07 21:46:24 +0800

    基础例子

--- demo/2016.08.15-c-plus-plus/getting-started/basic.cpp
+++ demo/2016.08.15-c-plus-plus/getting-started/basic.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
 
+// 基础入门例子
 int main () {
     cout << "Hello World !" << endl;
     return 0;    
 }
+
+// g++ 01.cpp
+// g++ 01.cpp -o 01.out
---
+++ demo/2016.08.15-c-plus-plus/getting-started/variable.cpp
@@ -0,0 +1,16 @@
+/*
+参考：http://www.runoob.com/cplusplus/cpp-data-types.html
+1、变量类型：bool char int float double void 
+2、修饰符：signed/unsigned、short、long
+3、不同变量类型所占内存空间、最大存储值：参考链接
+*/
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "Hello World !" << endl;
+    return 0;    
+}
+
+// g++ 01.cpp
+// g++ 01.cpp -o 01.out
