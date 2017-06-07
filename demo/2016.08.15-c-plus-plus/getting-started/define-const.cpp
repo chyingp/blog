9c36ab8aeb018e462b8f5977a16dbb61a87d619c
casperchen(陈映平) <chyingp@gmail.com>
2017-06-07 23:45:29 +0800

    define const 例子

---
+++ demo/2016.08.15-c-plus-plus/getting-started/define-const.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// 备注：后面不要加 ;
+#define PI 3.14
+
+int main () {
+    printf("PI IS %f \n", PI);
+
+    const int HEIGHT = 20;
+    printf("HEIGHT IS %d \n", HEIGHT);
+
+    return 0;    
+}
+
+// PI IS 3.140000
+// HEIGHT IS 20
