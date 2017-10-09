ad50c4e8cced1d04eafb6b6c70ac1b72045835e0
casperchen(陈映平) <chyingp@gmail.com>
2017-10-10 00:38:03 +0800

    例子：interface

---
+++ demo/2017.09.18-typescript/interface/first-interface.ts
@@ -0,0 +1,19 @@
+// function printLabel (labelObject: {label: string}):void {
+//   console.log(labelObject.label);
+// }
+
+// var labelObject = {nick: 'chyingp', label: 'label: chyingp'};
+// printLabel(labelObject);
+
+// printLabel({nick: 'chyingp', label: 'label: chyingp'});
+
+interface labelShape {
+  label: string
+}
+
+function printLabel(labelObject: labelShape): void {
+  console.log(labelObject.label);
+}
+
+var labelObject = {label: 'label: chyingp', nick: 'chyingp'};
+printLabel(labelObject);
---
+++ demo/2017.09.18-typescript/interface/optional-props.ts
@@ -0,0 +1,18 @@
+interface SquareConfig {
+  color?: string,
+  width?: number
+}
+
+function createSquare (config: SquareConfig): {color: string, area: number} {
+  let newSquare = {color: 'red', area: 100};
+  if (config.color) {
+    newSquare.color = config.color;
+  }
+  if (config.width) {
+    newSquare.area = config.width * config.width;
+  }
+  return newSquare;
+}
+
+let square = createSquare({color: 'blue', width: 20});
+console.log(square); // { color: 'blue', area: 400 }
