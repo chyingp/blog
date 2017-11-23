b1d007a873c9da8ea59a4f88f85cb06f4de7a6d9
casperchen(陈映平) <chyingp@gmail.com>
2017-11-23 14:54:21 +0800

    修改配置

---
+++ demo/2017.11.22-react-vue/react-demo/webpack.dev.js
@@ -0,0 +1,38 @@
+const WebpackShellPlugin = require('webpack-shell-plugin');
+// var ChunkManifestPlugin = require("chunk-manifest-webpack-plugin");
+// var WebpackChunkHash = require("webpack-chunk-hash");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	entry: './lib/app.js',  // 注意不要写 js/entry.js
+  output: {
+    path: __dirname,
+    filename: 'bundle/app.js'
+  },
+	module: {
+		rules: [
+			{ test: /\.css$/, use: 'css-loader' },
+			{
+	      test: /\.js$/,
+	      exclude: /(node_modules|bower_components)/,
+	      use: 'babel-loader'
+	    }
+		]
+	},
+	plugins: [		
+
+		new WebpackShellPlugin({
+			onBuildStart: ['echo "Webpack Start"'], 
+			onBuildEnd:[
+				'echo "Webpack End"',
+				'gulp'
+			]
+		}),
+
+		// new HtmlWebpackPlugin({			
+		// 	template: path.resolve(__dirname, 'page/app.ejs'), // Load a custom template (ejs by default see the FAQ for details)
+		// })
+	],
+};
