0f7322d69967fc517fb393fa1243c57497cf6482
casperchen(陈映平) <chyingp@gmail.com>
2017-11-23 10:44:39 +0800

    修改配置等

--- demo/2017.11.22-react-vue/react-demo/gulpfile.js
+++ demo/2017.11.22-react-vue/react-demo/gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var path = require('path');
 // var pug = require('gulp-pug');
 // var less = require('gulp-less');
 // var minifyCSS = require('gulp-csso');
@@ -9,14 +10,22 @@ var gulp = require('gulp');
 //     .pipe(gulp.dest('build/html'))
 // });
 
+var svrRoot = '/Users/a/Documents/code/develop/xunjiatong/InquiryHandNodeSvr/branches/2017.11.23-kh-m';
+
 gulp.task('views', function(){
-  return gulp.src(['page/*.html', 'page/*.ejs'])
-    .pipe(gulp.dest('../react-svr/views'))    
+  // let dest = '/Users/a/Documents/code/develop/xunjiatong/InquiryHandNodeSvr/branches/2017.11.10-group-inquiry/app/view/kh-m';
+  // let dest = '../react-svr/views';
+  let dest = path.join(svrRoot, 'app/view/kh-m');
+  return gulp.src(['page/*.html', 'page/*.ejs', 'build/*.html'])
+    .pipe(gulp.dest(dest));
 });
 
 gulp.task('public', function(){
-  return gulp.src('bundle/*.js')
-    .pipe(gulp.dest('../react-svr/public/bundle'))    
+  let dest = path.join(svrRoot, 'app/public/kh-m');
+  // let dest = '/Users/a/Documents/code/develop/xunjiatong/InquiryHandNodeSvr/branches/2017.11.10-group-inquiry/app/public/kh-m';
+  // let dest = '../react-svr/public/bundle';
+  return gulp.src('build/*.js')
+    .pipe(gulp.dest(dest));
 });
 
 // gulp.task('css', function(){
@@ -26,4 +35,6 @@ gulp.task('public', function(){
 //     .pipe(gulp.dest('build/css'))
 // });
 
-gulp.task('default', [ 'views', 'public' ]);
+gulp.task('default', 
+  [ 'views', 
+    'public' ]);
--- demo/2017.11.22-react-vue/react-demo/webpack.config.js
+++ demo/2017.11.22-react-vue/react-demo/webpack.config.js
@@ -1,11 +1,24 @@
 const WebpackShellPlugin = require('webpack-shell-plugin');
+var ChunkManifestPlugin = require("chunk-manifest-webpack-plugin");
+var WebpackChunkHash = require("webpack-chunk-hash");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
 	entry: './lib/app.js',  // 注意不要写 js/entry.js
+	// output: {
+	// 	path: __dirname,
+	// 	filename: 'bundle/app.js'
+	// },
 	output: {
-		path: __dirname,
-		filename: 'bundle/app.js'
+		path: path.resolve(__dirname, 'build'),
+		// publicPath: 'build',
+		filename: '[name].js'        
 	},
+	stats: {
+			hash: true
+	},	
 	module: {
 		rules: [
 			{ test: /\.css$/, use: 'css-loader' },
@@ -17,12 +30,32 @@ module.exports = {
 		]
 	},
 	plugins: [
-    new WebpackShellPlugin({
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor',
+			minChunks: function (module) {
+					// this assumes your vendor imports exist in the node_modules directory
+					return module.context && module.context.indexOf('node_modules') !== -1;
+			}
+		}),
+
+		new webpack.HashedModuleIdsPlugin(),
+		new WebpackChunkHash(),
+
+		new ChunkManifestPlugin({
+				filename: "chunk-manifest.json",
+				manifestVariable: "webpackManifest"
+		}),
+
+		new WebpackShellPlugin({
 			onBuildStart: ['echo "Webpack Start"'], 
 			onBuildEnd:[
 				'echo "Webpack End"',
 				'gulp'
 			]
-		})
-  ],
+		}),
+
+		new HtmlWebpackPlugin({			
+			template: path.resolve(__dirname, 'page/app.ejs'), // Load a custom template (ejs by default see the FAQ for details)
+		})  		
+	],
 };
