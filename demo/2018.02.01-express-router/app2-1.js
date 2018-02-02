0a877332e441a0faa4e9bbf0a3ad3d92f15fdbb8
casperchen(陈映平) <chyingp@gmail.com>
2018-02-02 16:09:21 +0800

    next('route') 对比

---
+++ demo/2018.02.01-express-router/app2-1.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.get('/user/:id', function (req, res, next) {
+  // if the user ID is 0, skip to the next route
+  if (req.params.id === '0') next('route')
+  // otherwise pass the control to the next middleware function in this stack
+  else next()
+}, function (req, res, next) {
+  // render a regular page
+  res.end('regular')
+})
+
+// handler for the /user/:id path, which renders a special page
+app.get('/user/:id', function (req, res, next) {
+  res.end('special')
+})
+
+app.listen(3000)
