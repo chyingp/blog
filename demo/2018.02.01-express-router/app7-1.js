0c540fdeb659f3a603f3db3a14780a8532b1e09e
casperchen(陈映平) <chyingp@gmail.com>
2018-02-03 23:52:24 +0800

    新增例子

--- demo/2018.02.01-express-router/app7-1.js
+++ demo/2018.02.01-express-router/app7-1.js
@@ -8,6 +8,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(function (req, res, next) {
+  res.render('index', {title: 'chyingp'}, function (error, html) {
+    console.log(html);
+    res.end(html);
+  });
+});
+
+app.use(function (req, res, next) {
   res.render('none_existed_view');
 });
 
