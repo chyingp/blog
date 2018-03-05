d7683905c878cd35ad5e76b455d06678f70af421
casperchen(陈映平) <chyingp@gmail.com>
2018-03-06 00:17:13 +0800

    修改注释

--- demo/2018.03.05-child_process-stdio/inherit/demo2.js
+++ demo/2018.03.05-child_process-stdio/inherit/demo2.js
@@ -2,7 +2,7 @@ const child_process = require('child_process');
 const spawn = child_process.spawn;
 
 const child = spawn('ls', ['1.txt', '2.txt'], {
-  stdio: 'inherit' // 默认
+  stdio: 'inherit'
 });
 
 console.log(child.stdout == null); // true
--- demo/2018.03.05-child_process-stdio/pipe/demo2.js
+++ demo/2018.03.05-child_process-stdio/pipe/demo2.js
@@ -5,5 +5,8 @@ const child = spawn('ls', ['1.txt', '2.txt'], {
   stdio: 'pipe' // 默认
 });
 
+// 通过pipe，将子进程的标准输出 打印到 父进程的 标准输出
 child.stdout.pipe(process.stdout);
+
+// 通过pipe，将子进程的标准错误输出 打印到 父进程的 标准错误输出
 child.stderr.pipe(process.stderr);
