038478f34aa472b334b887dd18f44c5437b39b85
casperchen(陈映平) <chyingp@gmail.com>
2018-03-06 00:17:04 +0800

    stdio: ignore 1

---
+++ demo/2018.03.05-child_process-stdio/ignore/demo1.js
@@ -0,0 +1,11 @@
+const child_process = require('child_process');
+const spawn = child_process.spawn;
+
+// 设置为ignore，导致
+// 1、父进程不会直接打印子进程的标准输出、标准错误输出
+// 2、子进程的标准输出、标准错误输出直接丢弃（参考命令 ls 1.txt 2.txt > /dev/null）
+const child = spawn('ls', ['1.txt', '2.txt'], {
+  stdio: 'ignore'
+});
+
+console.log( child.stdout ); // null
