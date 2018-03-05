8780ded36d3ddf133dfa488390722f2c221e22cd
casperchen(陈映平) <chyingp@gmail.com>
2018-03-06 00:09:34 +0800

    例子：stdio->pipe

---
+++ demo/2018.03.05-child_process-stdio/pipe/demo1.js
@@ -0,0 +1,18 @@
+const child_process = require('child_process');
+const spawn = child_process.spawn;
+
+const child = spawn('ls', ['1.txt', '2.txt'], {
+  stdio: 'pipe' // 默认
+});
+
+child.stdout.on('data', function (data) {
+  console.log(`child.stdout: ${data}`);
+});
+
+child.stderr.on('data', function (data) {
+  console.log(`child.stderr: ${data}`);
+});
+
+// 输出如下
+// child.stdout: 1.txt
+// child.stderr: ls: 2.txt: No such file or directory
