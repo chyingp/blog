33e9be37f085a894899d2e452cdf6888846b94b3
casperchen(陈映平) <chyingp@gmail.com>
2018-04-07 01:35:24 +0800

    视图例子

---
+++ demo/2018.04.06-koax/app.views.js
@@ -0,0 +1,12 @@
+const koa = require('koa');
+const app = new koa();
+const koaViews = require('koa-views');
+
+const views = koaViews(__dirname + '/views', { map: { html: 'ejs' }});
+app.use(views); // await ctx.render('profile', {nick: 'chyingp'});
+
+app.use(async (ctx, next) => {
+  await ctx.render(ctx.path.replace(/^\//, ''), {title: 'koa-views'});
+});
+
+app.listen(3000);
