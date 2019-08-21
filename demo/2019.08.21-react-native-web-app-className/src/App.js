75c70fd8b2a3669c21853a0a203c89eb654651eb
casperchen <chyingp@gmail.com>
2019-08-21 17:07:21 +0800

    add demo

---
+++ demo/2019.08.21-react-native-web-app-className/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {View, Text} from 'react-native';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload. 
+//         </p>
+//         <p>hello world</p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function App() {
+  return (
+    <View>
+      <Text>hello world</Text>
+    </View>
+  );
+}
+
+export default App;
