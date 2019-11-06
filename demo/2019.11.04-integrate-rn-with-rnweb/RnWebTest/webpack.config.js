e36276a34127cce01ba11a378afc3963e998c182
casperchen <chyingp@gmail.com>
2019-11-06 18:55:42 +0800

    添加编译配置

---
+++ demo/2019.11.04-integrate-rn-with-rnweb/RnWebTest/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+	entry: './index2.js',
+	output: {
+		filename: 'index.web.js',
+		path: path.resolve(__dirname, 'build-web'),
+	},
+	mode: 'development',
+    module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					// options: {
+					// 	presets: ['@babel/preset-env']
+					// }
+				}
+			}			
+		]
+	},
+
+  	resolve: {
+    	alias: {
+    		'react-native$': 'react-native-web'
+    	}
+  	}
+}
