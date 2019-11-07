037a54909628f4a322c97df7f8d8d1d8921af8c3
chyingp <416394284@qq.com>
2019-11-07 23:16:04 +0800

    添加入口文件

---
+++ demo/2019.11.07-integrate-rn-with-existing-ios-project/ios/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+
+class RNTest extends React.Component {
+  render() {
+    var contents = this.props['scores'].map((score) => (
+      <Text key={score.name}>
+        {score.name}:{score.value}
+        {'\n'}
+      </Text>
+    ));
+    return (
+      <View style={styles.container}>
+        <Text style={styles.highScoresTitle}>2048 High Scores!</Text>
+        <Text style={styles.scores}>{contents}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  highScoresTitle: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  scores: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+// Module name
+AppRegistry.registerComponent('RNTest', () => RNTest);
