d7b4dc6f20f5559ae9c57697d2af427a9fb1854f
chyingp <416394284@qq.com>
2019-11-08 13:59:53 +0800

    add new demo

---
+++ demo/2019.11.07-calling-native-module-from-js-in-rn/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {AppRegistry, StyleSheet, Text, View, Image} from 'react-native';
+
+class RNTest extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello 我是程序猿小卡.</Text>
+        <Image        
+          style={styles.avatar}
+          source={{uri: 'https://avatars3.githubusercontent.com/u/2383346?s=460&v=4'}}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: 60
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#333'
+  },
+  avatar: {
+    width: 300,
+    height: 300,
+    marginTop: 20
+  }
+});
+
+// Module name
+AppRegistry.registerComponent('RNTest', () => RNTest);
---
+++ demo/2019.11.07-calling-native-module-from-js-in-rn/ios/RNTest/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  RNTest
+//
+//  Created by casper on 2019/11/7.
+//  Copyright © 2019年 casper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction) rnTestButtonPressed:(id)sender;
+
+@end
+
