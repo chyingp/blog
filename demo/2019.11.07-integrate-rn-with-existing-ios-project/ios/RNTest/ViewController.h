7051a80ed93814e9d31d0dad97f588e007229825
chyingp <416394284@qq.com>
2019-11-08 10:28:56 +0800

    bug fix：让demo能跑起来

--- demo/2019.11.07-integrate-rn-with-existing-ios-project/ios/RNTest/ViewController.h
+++ demo/2019.11.07-integrate-rn-with-existing-ios-project/ios/RNTest/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction) rnTestButtonPressed:(id)sender;
 
 @end
 
--- demo/2019.11.07-integrate-rn-with-existing-ios-project/ios/RNTest/ViewController.m
+++ demo/2019.11.07-integrate-rn-with-existing-ios-project/ios/RNTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <React/RCTRootView.h>
 
 @interface ViewController ()
 
@@ -14,9 +15,37 @@
 
 @implementation ViewController
 
+- (IBAction)rnTestButtonPressed:(id)sender {
+    NSLog(@"RNTest Button Pressed");
+    NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.bundle?platform=ios"];
+    
+    RCTRootView *rootView =
+    [[RCTRootView alloc] initWithBundleURL: jsCodeLocation
+                                moduleName: @"RNTest"
+                         initialProperties:
+     @{
+       //       @"scores" : @[
+       //               @{
+       //                   @"name" : @"Alex",
+       //                   @"value": @"42"
+       //                   },
+       //               @{
+       //                   @"name" : @"Joel",
+       //                   @"value": @"10"
+       //                   }
+       //               ]
+       }
+                             launchOptions: nil];
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view = rootView;
+    [self presentViewController:vc animated:YES completion:nil];
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"viewDidLoad");
 }
 
 
