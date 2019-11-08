b780dc6655d26ab4558469d6afed9e2889a31db7
chyingp <416394284@qq.com>
2019-11-08 21:26:18 +0800

    promise example

--- demo/2019.11.07-calling-native-module-from-js-in-rn/index.js
+++ demo/2019.11.07-calling-native-module-from-js-in-rn/index.js
@@ -13,18 +13,29 @@ import {NativeModules} from 'react-native';
 const TodoList = NativeModules.TodoList;
 const todoListEmitter = new NativeEventEmitter(TodoList);
 
+// 例子：调用原生模块导出的方法
 TodoList.create();
 TodoList.add('起床');
+
+// 例子：回调
 TodoList.addWithCallback('吃早餐', (error, list) => {
   if (error === null) {
     console.log(`[addWithCallback] list.length == ${list.length}`);
   }
 });
 
+// 例子：事件监听
 todoListEmitter.addListener('ItemAdded', list => {
   console.log(`[ItemAdded] list.length == ${list.length}`);
 });
 TodoList.addAndTriggerEvent('上班');
+
+// 例子：返回Promise实例
+const resolver = msg => console.log(`[addAndReturnPromise] [resolved], ${msg}`);
+const rejecter = error => console.log(`[addAndReturnPromise] [rejected] ${error.message}`);
+    
+TodoList.addAndReturnPromise('加班', true).then(resolver).catch(rejecter);
+TodoList.addAndReturnPromise('休息', false).then(resolver).catch(rejecter);
 
 const CalendarManager = NativeModules.CalendarManager;
 const calendarManagerEmitter = new NativeEventEmitter(CalendarManager);
--- demo/2019.11.07-calling-native-module-from-js-in-rn/ios/RNTest/TodoList.m
+++ demo/2019.11.07-calling-native-module-from-js-in-rn/ios/RNTest/TodoList.m
@@ -20,14 +20,14 @@ RCT_EXPORT_METHOD(create)
     list = [[NSMutableArray alloc] init];
 }
 
-// 暴露方法
+// 例子：方法导出
 RCT_EXPORT_METHOD(add:(NSString *)item)
 {
     NSLog(@"add: %@", item);
     [list addObject:@{ @"desc":item, @"done": @0 }];
 }
 
-// 暴露方法，支持回调
+// 例子：方法导出，支持回调
 RCT_EXPORT_METHOD(addWithCallback:(NSString *)item callback:(RCTResponseSenderBlock)callback)
 {
     NSLog(@"addWithCallback: %@", item);
@@ -42,7 +42,7 @@ RCT_EXPORT_METHOD(addWithCallback:(NSString *)item callback:(RCTResponseSenderBl
     return @[@"ItemAdded"];
 }
 
-// 触发事件
+// 例子：事件抛出
 RCT_EXPORT_METHOD(addAndTriggerEvent:(NSString *)item)
 {
     NSLog(@"addAndTriggerEvent: %@", item);
@@ -51,20 +51,27 @@ RCT_EXPORT_METHOD(addAndTriggerEvent:(NSString *)item)
     [self sendEventWithName:@"ItemAdded" body:list];
 }
 
-RCT_REMAP_METHOD(getPromise,
+// 例子：返回Promise实例
+RCT_REMAP_METHOD(addAndReturnPromise,
+                 item:(NSString *) item
                  ifResolved:(int) ifResolved
                  findEventsWithResolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
+    NSLog(@"addAndTriggerEvent: %@", item);
+
+    [list addObject:@{ @"desc":item, @"done": @0 }];
+    
     if (ifResolved) {
-        resolve(@"success");
+        resolve([NSString stringWithFormat:@"%@ is accepted.", item]);
     } else {
         NSString *domain = @"com.chyingp.www";
         NSDictionary *userInfo = @{ @"desc": @"nonsense" };
         NSInteger code = -1;
         NSError *error =[NSError errorWithDomain:domain code:code userInfo:userInfo];
+        NSString *errMsg = [NSString stringWithFormat:@"%@ is not accepted.", item];
         
-        reject(@"ErrorFromNativeModule", @"Error occurred.", error);
+        reject(@"ErrorFromNativeModule", errMsg, error);
     }
 }
 
