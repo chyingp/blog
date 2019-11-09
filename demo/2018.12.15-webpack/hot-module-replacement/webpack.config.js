734675faa4b1e01da527023c92e4577661fff1cd
chyingp <chyingp@gmail.com>
2019-11-09 10:52:46 +0800

    webpack example

---
+++ demo/2018.12.15-webpack/hot-module-replacement/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  }
+};
