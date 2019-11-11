1ca8a0c199ac87b6e4914ab2a80369cc716a4d29
chyingp <chyingp@gmail.com>
2019-11-11 22:53:18 +0800

    hermesvm example

---
+++ demo/2019.11.07-rn-experiment/ios/RCTConvert+Hello.m
@@ -0,0 +1,40 @@
+//
+//  RCTConvert+Mapkit.m
+//  RNTest
+//
+//  Created by casperchen on 2019/11/9.
+//  Copyright © 2019年 casper. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+#import <React/RCTConvert.h>
+#import <CoreLocation/CoreLocation.h>
+#import <React/RCTConvert+CoreLocation.h>
+
+@interface RCTConvert (Mapkit)
+
++ (MKCoordinateSpan)MKCoordinateSpan:(id)json;
++ (MKCoordinateRegion)MKCoordinateRegion:(id)json;
+
+@end
+
+@implementation RCTConvert(MapKit)
+
++ (MKCoordinateSpan)MKCoordinateSpan:(id)json
+{
+    json = [self NSDictionary:json];
+    return (MKCoordinateSpan){
+        [self CLLocationDegrees:json[@"latitudeDelta"]],
+        [self CLLocationDegrees:json[@"longitudeDelta"]]
+    };
+}
+
++ (MKCoordinateRegion)MKCoordinateRegion:(id)json
+{
+    return (MKCoordinateRegion){
+        [self CLLocationCoordinate2D:json],
+        [self MKCoordinateSpan:json]
+    };
+}
+
+@end
---
+++ demo/2019.11.07-rn-experiment/ios/RNTMapView.h
@@ -0,0 +1,13 @@
+//
+//  RNTMapView.h
+//  RNTest
+//
+//  Created by casperchen on 2019/11/9.
+//  Copyright © 2019年 casper. All rights reserved.
+//
+
+#ifndef RNTMapView_h
+#define RNTMapView_h
+
+
+#endif /* RNTMapView_h */
