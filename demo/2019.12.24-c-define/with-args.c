612759ca12108424ffe6afd76bf3ee4fbbb764cb
chyingp <chyingp@gmail.com>
2019-12-24 10:05:52 +0800

    feat: c define macro

---
+++ demo/2019.12.24-c-define/with-args.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define SUM(a, b) a + b
+
+int main() 
+{
+    int ret = SUM(10, 20);
+    printf("sum is %d", ret);
+    return 0;
+}
---
+++ demo/2019.12.24-c-define/without-args.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define COUNT 1000
+
+int main() 
+{
+    printf("count is %d", COUNT);
+    return 0;
+}
