e1395fa0b9e963af19d22a82560266ef40248e28
chyingp <416394284@qq.com>
2022-02-12 23:34:43 +0800

    feat: useEffect implemented

--- demo/2022.02.12-react-hooks/index.js
+++ demo/2022.02.12-react-hooks/index.js
@@ -2,7 +2,8 @@
 var renderNode = {
 	current: null,
 	container: null,
-	stateIndex: 0
+	stateIndex: 0,
+	effectIndex: 0
 };
 
 const React = {
@@ -33,7 +34,9 @@ const React = {
 			type: type,
 			props: props,
 			stateList: [],
+			effectList: [],
 			eventMap: eventMap,
+			mouted: false,
 			render: render
 		};
 
@@ -56,26 +59,61 @@ const React = {
 				ReactDom.render(_renderNode.current, _renderNode.container);
 			}
 		];
+	},
+
+	useEffect(callback, deps) {
+		const _renderNode = renderNode;
+		const _effectIndex = _renderNode.effectIndex;
+
+		_renderNode.current.effectList[_effectIndex] = {callback, deps};
+		_renderNode.effectIndex++;
 	}
 };
 
 const ReactDom = {
 	render(reactElement, container) {
+		// 跳过state对比的环节，可能导致死循环，比如在useEffect里直接setState
 
 		renderNode.current = reactElement
 		renderNode.container = container;
 		renderNode.stateIndex = 0;
+		renderNode.effectIndex = 0;
 
 		container.innerHTML = '';
 		container.appendChild(reactElement.render());
+
+		reactElement.effectList.forEach(({callback, deps}) => {
+
+			if (reactElement.mouted === false) {
+				callback();
+			} else {
+				if (!deps || deps.length !== 0) {
+					callback();
+				}
+			}
+		});
+
+		reactElement.mouted = true;
 	}
 };
 
-const useState = React.useState;
+const { useState, useEffect } = React;
 
 function App() {
 	const [count, setCount] = useState(0);
 	const [number, setNumber] = useState(1);
+
+	useEffect(() => {
+		console.log('[1] useEffect is called, and will be called only once.');
+
+		setTimeout(() => {
+			setCount(count + 1);
+		}, 3000);
+	}, []);
+
+	useEffect(() => {
+		console.log('[2] useEffect is called, and will be called when mounted and updated.');
+	});
 
 	return React.createElement('div', {
 		innerHTML: `
